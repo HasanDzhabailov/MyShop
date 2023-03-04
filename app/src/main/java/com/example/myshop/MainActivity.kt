939f37a8315fcf05b44ce0b0c144a44525e32aae
Hasan <hasandzhabrailov@vk.com>
2023-03-04 21:55:44 +0300

    completed layout Home screen

--- app/src/main/java/com/example/myshop/MainActivity.kt
+++ app/src/main/java/com/example/myshop/MainActivity.kt
@@ -6,31 +6,54 @@ import android.util.Log
 import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.example.myshop.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+	private val binding: ActivityMainBinding by lazy {
+		ActivityMainBinding.inflate(layoutInflater)
+	}
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		binding.bottomNavigationView.setOnItemSelectedListener { item ->
+			when (item.itemId) {
+				com.example.navigation.R.id.home_screen -> {
+					return@setOnItemSelectedListener true
+				}
+				com.example.feature_profile_screen.R.id.profile_screen ->{
+					return@setOnItemSelectedListener true
+				}
+			}
+			false
+		}
 		setupNav()
 	}
-	private fun setupNav(){
-		val navHostFragment  = supportFragmentManager.findFragmentById(R.id.main_nav_graph) as NavHostFragment
+
+	private fun setupNav() {
+		val navHostFragment =
+			supportFragmentManager.findFragmentById(R.id.main_nav_graph) as NavHostFragment
 		val navController = navHostFragment.navController
 
-		navController.addOnDestinationChangedListener{_, destination,_ ->
+		navController.addOnDestinationChangedListener { _, destination, _ ->
 
-			when(destination.id){
-			com.example.navigation.R.id.home_screen - 1  -> showBottomNav()
-			com.example.navigation.R.id.profile_screen - 1  -> showBottomNav()
-			com.example.navigation.R.id.product_details_screen - 1 -> showBottomNav()
-			else -> hideBottomNav() }
+			when (destination.id) {
+				com.example.navigation.R.id.home_screen - 1 -> showBottomNav()
+				com.example.navigation.R.id.profile_screen - 1 -> showBottomNav()
+				com.example.navigation.R.id.product_details_screen - 1 -> showBottomNav()
+				else -> hideBottomNav()
+			}
 		}
-		}
-	private fun showBottomNav(){
+
+
+	}
+
+	private fun showBottomNav() {
 		findViewById<BottomNavigationView>(R.id.bottom_navigation_view).visibility = View.VISIBLE
 	}
-	private fun hideBottomNav(){
+
+	private fun hideBottomNav() {
 		findViewById<BottomNavigationView>(R.id.bottom_navigation_view).visibility = View.GONE
 	}
 
---
+++ feature-home-screen/src/main/java/com/example/feature_home_screen/presentation/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.feature_home_screen.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.core.common.BaseFragment
+import com.example.feature_home_screen.R
+import com.example.feature_home_screen.databinding.FragmentHomeBinding
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+	}
+
+}
